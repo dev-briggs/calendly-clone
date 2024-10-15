1efbb4dbde93dec35bae728363cf4d7fb5c6a72c
Briggs Victoria <dev.briggs.victoria@gmail.com>
2024-10-15 18:37:30 +0200

    ✨ feat(events-page, drizzle-db): update events page
    -add db export
    - add redirect logic for null userId
    - add query for events
    - add page content

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,13 @@
-import { drizzle } from 'drizzle-orm/connect';
-async function main() {
-    const db = await drizzle("neon-http", process.env.DATABASE_URL);
-}
-main();
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+// async function main() {
+//   const sql = neon(process.env.DATABASE_URL!);
+//   const db = drizzle(sql, { schema });
+// }
+
+// main();
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
